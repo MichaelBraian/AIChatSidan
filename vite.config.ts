409c982b19ff5c7fda19097f4c8d7af41c32597c
Michael Braian <michaelbraian@Michaels-MBP.lan>
2024-10-23 20:00:51 +0200

    Update vite config and fix Firebase initialization

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://michaelbraian.netlify.app/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  define: {
+    'process.env': process.env
+  }
+})
